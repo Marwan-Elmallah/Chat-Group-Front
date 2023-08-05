000deb8276bc85cc3edc06b5fd62fb7ca1ff5ed9
Marwan-Elmallah <engmarwan0110@gmail.com>
2023-08-05 06:28:53 +0300

    update commit

--- JS/index.js
+++ JS/index.js
@@ -7,6 +7,12 @@ const sendMsgBtn = document.getElementById("sendMsgBtn")
 const message = document.getElementById("message")
 const chatDisplay = document.getElementById("chatDisplay")
 
+const { Server } = require("socket.io");
+
+const io = new Server({
+    serveClient: false
+});
+
 const clientIO = io("https://chat-group-aqmx.onrender.com")
 
 clientIO.emit("connection")
