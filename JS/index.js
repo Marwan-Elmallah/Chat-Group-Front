b6858ef6419b205c0ed349171cbaf971cd1eaec4
Marwan-Elmallah <engmarwan0110@gmail.com>
2023-08-05 22:30:37 +0300

    first commit

--- JS/index.js
+++ JS/index.js
@@ -6,8 +6,11 @@ const chatRoom = document.getElementById("chatRoom")
 const sendMsgBtn = document.getElementById("sendMsgBtn")
 const message = document.getElementById("message")
 const chatDisplay = document.getElementById("chatDisplay")
+const chatRoomPrivate = document.getElementById("chatRoomPrivate")
 
 const clientIO = io("https://chat-group-aqmx.onrender.com")
+
+// const clientIO = io("http://localhost:5000")
 
 clientIO.emit("connection")
 
@@ -47,7 +50,7 @@ clientIO.on("showUsers", (users) => {
     usersList.innerHTML = ""
     users.map((user) => {
         usersList.innerHTML += `
-        <li class="list-group-item px-4">${user.username}</li>
+        <li class="list-group-item px-4" onClick="chatTo('${user.id}')">${user.username}</li>
     `
     })
 })
@@ -57,8 +60,50 @@ sendMsgBtn.addEventListener("click", () => {
     message.value = ""
 })
 
-
-
 allChat()
+
+// Private Chat
+
+const usersListPrivate = document.getElementById("usersListPrivate")
+const chatDisplayPrivate = document.getElementById("chatDisplayPrivate")
+const messagePrivate = document.getElementById("messagePrivate")
+const sendMsgBtnPrivate = document.getElementById("sendMsgBtnPrivate")
+
+
+let chatTo = (id) => {
+    console.log(id);
+    clientIO.emit("chatWith", id)
+    chatRoom.style.display = "none"
+    chatRoomPrivate.style.display = "block"
+}
+clientIO.on("showUser", (data) => {
+    console.log(data);
+    usersListPrivate.innerHTML += `
+    <li class="list-group-item px-4">${data}</li>
+    `
+})
+
+sendMsgBtnPrivate.addEventListener("click", () => {
+    clientIO.emit("sendMessagePrivate", messagePrivate.value)
+    messagePrivate.value = ""
+    chatPrivate()
+})
+
+const chatPrivate = () => {
+    clientIO.on("displayMsgPrivate", (msg) => {
+        chatDisplayPrivate.innerHTML = ""
+        msg.map((m) => {
+            chatDisplayPrivate.innerHTML += `
+                <div class="card my-3 p-2">
+                    <h6 class="card-subtitle mb-2 text-primary">From: ${m.user}</h6>
+                    <p class="card-text">${m.message}</p>
+                </div>
+            `
+        })
+        // console.log(msg);
+    })
+}
+
+chatPrivate()
 
 
